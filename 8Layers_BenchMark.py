260662e8b17450e50f0d1fa0fc244e9267574dee
Mohit645 <mohit645@outlook.com>
2019-10-16 14:46:17 +0100

    Replicated the benchMark model

---
+++ 8Layers_BenchMark.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 01:39:28 2019
+
+@author: Mohit
+"""
+import os
+import shutil
+import cv2
+file_repository="C:\\Users\\Mohit\\OneDrive\\Documents\\Sem3\\AllSegmentationGroundtruths\\"
+os.chdir(file_repository)
+
+import os
+from glob import glob
+from PIL import Image
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+from numpy import expand_dims
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+
+
+
+#Copying files to new location and making new repository
+
+path ="C:\\Users\\Mohit\\OneDrive\\Documents\\Sem3\\NewSegmentation\\"
+os.chdir(path)
+newClasses = ['HE\\', 'Background\\']
+
+for i in newClasses:
+    os.mkdir(path+i)
+
+set1 = ["Testing Set\\", 'Training Set\\']
+oldClasses = ['1. Microaneurysms\\', '2. Haemorrhages\\', '3. Hard Exudates\\', '4. Soft Exudates\\', '5. Optic Disc\\']
+
+#Reading the images
+
+
+for i in set1:
+    for j in oldClasses:
+        images = [f for f in os.listdir(file_repository+i+j) if os.path.splitext(f)[-1] == '.tif']
+        for k in images:
+            src_dir = file_repository+i+j+k
+            if(j=='3. Hard Exudates\\'):
+                dest_dir = path+newClasses[0]
+                
+            else:
+                dest_dir = path+newClasses[1]
+            shutil.copy(src_dir,dest_dir)
+
+#Resizing the images(256*256)
+for j in newClasses:
+    images = [f for f in os.listdir(path+j) if os.path.splitext(f)[-1] == '.tif'] 
+    for i in images:    
+        img = Image.open(path+j+i)
+        img = img.resize((256,256), Image.ANTIALIAS)
+        img.save(path+j+i)
+
+
+#DataAugmentation
+images = [f for f in os.listdir(path+newClasses[0]) if os.path.splitext(f)[-1] == '.tif']          
+itr = round(100000/len(images))
+
+for image in images:
+        img = load_img(path+'HE\\'+image) 
+        data = img_to_array(img)
+        samples = expand_dims(data, 0)
+        datagen = ImageDataGenerator(rotation_range=90)
+        it = datagen.flow(samples, batch_size=32, save_to_dir=path+'HE\\' ,save_prefix='AUG_ISIC_GEN_'+image, save_format='jpg')
+        for j in range(itr):
+            batch = it.next()
+            
+            
+
+images = [f for f in os.listdir(path+newClasses[1]) if os.path.splitext(f)[-1] == '.tif']  
+itr =  round(100000/len(images)) 
+
+for image in images:
+        img = load_img(path+'Background\\'+image) 
+        data = img_to_array(img)
+        samples = expand_dims(data, 0)
+        datagen = ImageDataGenerator(rotation_range=90)
+        it = datagen.flow(samples, batch_size=32, save_to_dir=path+'Background\\' ,save_prefix='AUG_ISIC_GEN_'+image, save_format='jpg')
+        for j in range(itr):
+            batch = it.next()
+     
+#Creating test and training sets
+os.mkdir(path+'Train\\')
+os.mkdir(path+'Test\\')
+
+from sklearn.model_selection import train_test_split
+for i in newClasses:
+    newPath =path+i
+    images = [f for f in os.listdir(newPath) if os.path.splitext(f)[-1] == '.jpg' or os.path.splitext(f)[-1] == '.tif']
+    train,test = train_test_split(images, test_size = 1/4, random_state = 0)
+    
+    dst_dir = path+"Test\\"+i
+    os.mkdir(dst_dir)
+    
+    for k in test:
+        src_dir=path+i+k
+        shutil.copy(src_dir,dst_dir)
+    del(images)
+    
+
+    
+    for j in train:
+        src_dir=path+i+j
+        shutil.copy(src_dir,dst_dir)
+    
+
+
+
+import keras
+from keras.layers import MaxPool2D
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D,AveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import LearningRateScheduler,ReduceLROnPlateau
+from keras.optimizers import Adam 
+from keras.preprocessing.image import ImageDataGenerator # to augmenting our images for increasing accuracy
+from keras.utils.vis_utils import plot_model
+import scipy
+from sklearn.model_selection import train_test_split # to split our train data into train and validation sets
+import numpy as np 
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+np.random.seed(13) 
+
+
+
+
+num_classes = 2 
+batch_size = 50 # taken from benchmark
+epochs = 20 # 20 Epoch is enough
+img_rows, img_cols = 32, 32 
+input_shape = (img_rows, img_cols,3) # We'll use this while building layers
+
+model = Sequential()
+model.add(Conv2D(32,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(32,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(64,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(64,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(128,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(128,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(256,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(256,(3,3), activation = 'relu', input_shape =input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(1,1),padding='same'))
+model.add(Dropout(0.2))
+
+# To be able to merge into fully connected layer we have to flatten
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(1000, activation="relu"))
+#Second fully connected layer
+model.add(Dense(100, activation="relu"))
+#activation 
+model.add(Dense(num_classes, activation = "sigmoid"))
+#model.add(Dense(num_classes, activation = "softmax"))
+# Compile the model with loss and metrics
+model.compile(optimizer =  Adam() , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+
+
+
+#Step6 Fitting images
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+from keras.callbacks import ReduceLROnPlateau
+annealer  = ReduceLROnPlateau(monitor='val_acc')
+
+import os
+path ="C:\\Users\\Mohit\\OneDrive\\Documents\\Sem3\\NewSegmentation\\"
+os.chdir(path)
+
+
+training_set = train_datagen.flow_from_directory('Train', target_size=(32,32), batch_size=batch_size, class_mode='categorical')
+
+test_set = test_datagen.flow_from_directory('Test', target_size=(32,32), batch_size=batch_size, class_mode='categorical')
+#We added the loss function for the model. Since we have multiple classes so we used categorical crossentropy function.
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit_generator(training_set,
+                       steps_per_epoch=144659,
+                       epochs=epochs,
+                       validation_data=test_set,
+                       validation_steps=48221,
+                       callbacks=[annealer])
